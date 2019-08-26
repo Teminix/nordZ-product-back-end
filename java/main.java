5fb7159926b5ae24fef4f406ecbd44155f9328e6
BuildTools <kabirdesarkar@gmail.com>
2019-08-26 20:34:37 +0530

    Added migrated files(mostly)

---
+++ java/main.java
@@ -0,0 +1,11 @@
+/*
+Here is the main java file
+
+Main code goes Here
+Add more java files/folders/classes or whatever you need
+*/
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
+}
