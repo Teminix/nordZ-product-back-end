be8e82a81df3ca5f80f38984bd819f88518b2aba
BuildTools <kabirdesarkar@gmail.com>
2019-08-27 13:11:20 +0530

    Added scanner

--- java/main.java
+++ java/main.java
@@ -4,12 +4,13 @@ Here is the main java file
 Main code goes Here
 Add more java files/folders/classes or whatever you need
 */
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("ghrgorhg");
 System.out.println("dfffffdhsdhwsudbhwubd");
-
+	
     String var = "racial slurs";
   }
 }
